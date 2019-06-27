970461dbb1cf78cb2a5338ba100df0b271714bd4
AnikinKostya <52211374+AnikinKostya@users.noreply.github.com>
2019-06-27 15:45:40 +0400

    Add files via upload

---
+++ Lines/Lines/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = "простой простой,. текст для.. примера., примера для1.. задания по по Строкам".ToLower().Split(' '); // меняем заглавные буквы и делим на подстроки
+            int n = 1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = str[i].Trim('.', ',');  // убираем с текста . и ,
+            }
+            var val = str.Where(x => str.Count(z => z == x) == n).Distinct(); // создаем переменную, которая выбирает не повторяющиеся слова
+            Console.WriteLine(string.Join(" ", val));
+            Console.ReadKey();
+        }
+    }
+}
